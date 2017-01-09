4f29ca9e1927e3bae54e319bebd025c6da386134
Farruco Sanjurjo <madtrick@gmail.com>
2017-01-09 14:12:49 +0100

    apply transformation chains

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from 'express'
 import middleware from './lib/middleware'
 import axios from 'axios'
-import * as FS_ from 'fs'
-import * as B from 'bluebird'
-
-const FS = B.promisifyAll(FS_)
-const port = process.env.PORT || 3000
+import resolveRespose from 'contentful-resolve-response'
+import * as VM from 'vm'
 
 const dispatcherId = process.env['DISPATCHER_ID']
 
@@ -13,10 +10,29 @@ if (!dispatcherId) {
   throw new Error('Missing "DISPATCHER_ID" env variable')
 }
 
+function dispatch (dispatcher, entry) {
+  const chains = dispatcher.fields.chains
+  const applicableChains = chains.filter((chain) => chain.fields.contentType === entry.sys.contentType.sys.id)
+  const transformations = applicableChains.reduce((transformations, chain) => {
+    const compiledTransformations = chain.fields.transformationFunctions.map((transformation) => {
+      return new VM.Script(transformation.fields.code)
+    })
+
+    return transformations.concat(compiledTransformations)
+  }, [])
+
+  return transformations.reduce((trsfEntry, transformation) => {
+    const module = { exports: {} }
+
+    transformation.runInNewContext({module})
+
+    return module.exports(trsfEntry)
+  }, entry)
+}
+
 express()
 .use(middleware(function* (req) {
   const authHeader = req.headers['authorization']
-  const trsfId = req.query.transformation
   const path = req.path.substr(1).split('/')
   const space = path[1]
 
@@ -29,33 +45,32 @@ express()
     },
   })
 
-  const [trsfVersion, transformSrc] = yield* getTransformation(space, trsfId, authHeader)
-  const fileName = `./tmp/${space}-${trsfId}-${trsfVersion}.js`
-  console.log(`Writing transformation to ${fileName}`)
-  yield FS.writeFileAsync(fileName, transformSrc)
-  const transform = require(fileName)
+  const dispatcher = yield* loadDispatcher(space, authHeader)
+  const transformedEntry = dispatch(dispatcher, response.data)
 
   return {
     status: 200,
-    body: JSON.stringify(transform(response.data), null, 2),
+    body: JSON.stringify(transformedEntry, null, 2),
   }
 }))
 .listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
 
-
-function* getTransformation (space, id, authHeader) {
+function* loadDispatcher (space, authHeader) {
   const response = yield axios({
     method: 'GET',
     baseURL: 'https://cdn.contentful.com',
-    url: `/spaces/${space}/entries/${id}`,
+    url: `/spaces/${space}/entries`,
+    params: {
+      'sys.id': dispatcherId,
+      include: 2,
+    },
     headers: {
-      'Authorization': authHeader,
+      Authorization: authHeader,
     },
   })
-  return [
-    response.data.sys.revision,
-    response.data.fields.code,
-  ]
+  const dispatcher = resolveRespose(response.data)[0]
+
+  return dispatcher
 }
